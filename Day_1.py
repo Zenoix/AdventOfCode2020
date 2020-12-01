291bc0dfcb23d2d8446f223d60b67692cfce1d6d
Jeff Wang <jeff.wang0296@gmail.com>
2020-12-01 18:33:20 +1300

    Create Day_1.py

---
+++ Day_1.py
@@ -0,0 +1,18 @@
+import itertools
+from functools import reduce
+import operator
+
+report = []
+with open("report.txt", "r") as f:
+    for x in f:
+        report.append(int(x.rstrip()))
+        
+n = int(input("How many numbers to sum and multiply? "))
+combs = list(itertools.combinations(report, n))
+
+for combination in combs:
+    if sum(combination) == 2020:
+        print(reduce(operator.mul, combination, 1))
+        break
+else:
+    print("Combination do not exist")
