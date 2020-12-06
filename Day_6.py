5da8236029a4bf0fdc423b550f8373a464c5210a
Zenoix <jeff.wang0296@gmail.com>
2020-12-06 22:06:29 +1300

    Cleaned up code and added comments

--- Day_6.py
+++ Day_6.py
@@ -1,32 +1,60 @@
+# import Counter
 from collections import Counter
 
 
-def part_1():
+# function to handle the input into a initially unclean list
+def handle_input():
     with open("input.txt", "r") as f:
         data = f.read()
-        groups = [group.replace("\n", "") for group in data.split("\n\n")]
-        distinct_letters = [len(set(group)) for group in groups]
-        return sum(distinct_letters)
+        # seperate the groups
+        groups_list = data.split("\n\n")
+    return groups_list
 
 
-def part_2():
-    with open("input.txt", "r") as f:
-        data = f.read()
-        groups = data.split("\n\n")
-        people = [group.split() for group in groups]
-        lis = []
-        output = 0
-        for group in people:
-            a = Counter()
-            for person in group:
-                a += Counter(person)
-            lis.append(a)
-        for index, counter in enumerate(lis):
-            for v in counter.values():
-                if v == len(people[index]):
-                    output += 1
-        return output
+# part 1 function
+def part_1() -> int:
+    unclean_groups = handle_input()
+    # remove "\n" characters in the list elements
+    groups = [group.replace("\n", "") for group in unclean_groups]
+    # find number of destinct letters
+    distinct_letters = [len(set(group)) for group in groups]
+    return sum(distinct_letters)
 
 
-print(part_1())
-print(part_2())
+# part 2 function
+def part_2() -> int:
+    unclean_groups = handle_input()
+    # seperate the each group into arrays of people's ans
+    people = [group.split() for group in unclean_groups]
+
+    # list to store the number of yeses to a qu for each group
+    group_yes = []
+    # output variable
+    output = 0
+
+    # loop through the groups
+    for group in people:
+        # create a counter for the number of yeses to a question
+        yes_ans = Counter()
+        # loop through each person in the group
+        for person in group:
+            # add the occurances of the yeses to a question
+            yes_ans += Counter(person)
+        # append the total number of yeses for each question
+        group_yes.append(yes_ans)
+
+    # loop through total number of yeses for each question per group
+    for idx, counter in enumerate(group_yes):
+        # check the number of yeses for each question
+        for v in counter.values():
+            # if the number of yeses == number of people in group
+            if v == len(people[idx]):
+                # add 1 to output
+                output += 1
+    return output
+
+
+# driver code
+if __name__ == "__main__":
+    print("Part 1", part_1())
+    print("Part 2", part_2())
