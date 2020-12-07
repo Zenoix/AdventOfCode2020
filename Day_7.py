0ccbe6aef5e32af331b1221359ce0157a6a7e06d
Zenoix <jeff.wang0296@gmail.com>
2020-12-08 02:42:13 +1300

    completed day 7 and added comments

--- Day_7.py
+++ Day_7.py
@@ -1,53 +1,92 @@
+# import regex
 import re
 
 
-def handle_input():
-    with open("test.txt", "r") as f:
+# function to create a list from input
+def handle_input() -> list[str]:
+    with open("input.txt", "r") as f:
+        # read in the input file as create a list without \n
         rules = f.read().splitlines()
+
     return rules
 
 
-def rules_to_dict(rules):
+# create a dictionary for each bag and it's inner bags and amount
+# for part 2
+def rules_to_dict(rules: list[str]) -> dict:
+    # initialise output dictionary
     output = {}
+
+    # loop through the lists
     for rule in rules:
+        # the outer bag's description
         outer = re.findall(r"^(.+?) bag", rule)
-        print(outer)
+        # the inner bag's description
         inner = re.findall(r"\d (.+?) bag", rule)
-        amount = re.findall(r"(\d+)", rule)
+        # the amount that each inner bag can carry converted to ints
+        amount = map(int, re.findall(r"(\d+)", rule))
+        # the output dictionary has the key as the outer bag's desc and the
+        # value as a nested dictionary of the inner bag's desc and num of bags
         output[outer[0]] = {k: v for k, v in zip(inner, amount)}
-        
+
     return output
 
 
-def part_1(rules):
+# part 1 code
+def part_1(rules: list[str]) -> int:
+    # initialise empty output set
     output = set()
-    current = set()
-    while not output or current:
-        previous = current.copy()
-        output |= previous
-        current = set()
+    # let outer be the bags that can carry the inner bags
+    outer = set()
+    # loop while output is empty or if outer has bags in it
+    while not output or outer:
+        # let inner be the previous outer bags that could carry the previous
+        # iteration of inner bags
+        inner = outer.copy()
+        # add the bags into output
+        output |= inner
+        # empty the set of bags that can carry the inner bags
+        outer = set()
 
-        if not previous:
-            previous = ["shiny gold"]
+        # if this is the first iteration (output is empty)
+        # let the inner bag be "shiny gold"
+        if not output:
+            inner = ["shiny gold"]
+        # loop through each rule
         for rule in rules:
-            for a in previous:
-                add = re.findall("^(.+) bags .+%s" % a, rule)
+            # loop through each of the inner bags that might be carriable by
+            # the current bag we are checking in the rule
+            for bag in inner:
+                # if the inner bag is in the rule but not at the beginning
+                add = re.findall("^(.+) bags .+%s" % bag, rule)
+                # add it to outer set
                 if add:
-                    current.add(add[0])
+                    outer.add(add[0])
 
     return len(output)
 
 
-def part_2(rules: dict):
-    if len(rules[next(iter(rules))]) == 0:
-        return
-    return
+# part 2 code
+def part_2(key: str) -> int:
+    # initialise the total amount of required inside the shiny gold bag
+    output = 0
+
+    # use recursion to find total number of bags inside of each bag
+    # (I don't know how to explain this part)
+    for inner_color, num_of_bags in bag_dict[key].items():
+        output += num_of_bags * (1 + part_2(inner_color))
+
+    return output
 
 
+# driver code
 if __name__ == "__main__":
+    # get the rules as a list first
     rules = handle_input()
-    rules_to_dict(rules)
-    #print(part_1(rules))
-    #print(part_2(rules))
-        
+    # part 1 answer
+    print("Part 1:", part_1(rules))
 
+    # convert the list of rules into a dictionary
+    bag_dict = rules_to_dict(rules)
+    # part 2 answer with starting key as "shiny gold"
+    print("Part 2:", part_2("shiny gold"))
