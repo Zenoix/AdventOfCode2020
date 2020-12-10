8172485254f56ed17922c65c87af8dde3705c437
Zenoix <jeff.wang0296@gmail.com>
2020-12-10 17:37:39 +1300

    Made adjustments

--- template.py
+++ template.py
@@ -1,5 +1,5 @@
 # function to create a list from input
-def handle_input() -> list[str]:
+def handle_input():
     with open("test.txt", "r") as f:
         # read in the input file as create a list without \n
         output = f.read().splitlines()
@@ -8,19 +8,19 @@ def handle_input() -> list[str]:
 
 
 # part 1 code
-def part_1():
+def part_1() -> int:
     pass
 
 
 # part 2 code
-def part_2(key: str) -> int:
+def part_2() -> int:
     pass
 
 
 # driver code
 if __name__ == "__main__":
 
-    foo = handle_input()
+    handle_input()
 
     print("Part 1:", part_1())
     print("Part 2:", part_2())
